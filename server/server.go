66638a9157d79f95c2759dea2d71718c97eb1074
Kenta <kenta0304terai@gmail.com>
2025-01-28 02:43:24 +0100

    feat: add cors

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	storage_go "github.com/supabase-community/storage-go"
 	"gorm.io/gorm"
@@ -13,6 +16,17 @@ type Server struct {
 }
 
 func NewServer(supabaseClient *storage_go.Client, postgrestDB *gorm.DB) *Server {
+
+	app := gin.Default()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	return &Server{
 		Router:         gin.Default(),
 		SupabaseClient: supabaseClient,
